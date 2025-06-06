7cb8ef89a2754ad3f4fd56f1dfb92d48c84f4f1c
Víctor Blanco <34665275+Blanquitoh@users.noreply.github.com>
2025-06-05 20:25:18 -0400

    Register validators and validate requests in front-end

--- Sakila.Web/Extensions/ServiceCollectionExtensions.cs
+++ Sakila.Web/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using FluentValidation;
 using Sakila.Contracts.Services;
+using Sakila.Contracts.Languages.Commands;
+using Sakila.Contracts.Languages.Validators;
+using Sakila.Contracts.Countries.Commands;
+using Sakila.Contracts.Countries.Validators;
 using Sakila.Web.Common;
 using Sakila.Web.Services;
 
@@ -12,6 +17,12 @@ public static class ServiceCollectionExtensions
         builder.Services.AddScoped<IApiClient, ApiClient>();
         builder.Services.AddScoped<ILanguageService, LanguageService>();
         builder.Services.AddScoped<ICountryService, CountryService>();
+        builder.Services.AddTransient<IValidator<LanguageCreateRequest>, LanguageCreateValidator>();
+        builder.Services.AddTransient<IValidator<LanguageUpdateRequest>, LanguageUpdateValidator>();
+        builder.Services.AddTransient<IValidator<LanguageDeleteRequest>, LanguageDeleteValidator>();
+        builder.Services.AddTransient<IValidator<CountryCreateRequest>, CountryCreateValidator>();
+        builder.Services.AddTransient<IValidator<CountryUpdateRequest>, CountryUpdateValidator>();
+        builder.Services.AddTransient<IValidator<CountryDeleteRequest>, CountryDeleteValidator>();
         builder.Services.AddScoped(sp => new HttpClient
             { BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"]!) });
         return builder;
