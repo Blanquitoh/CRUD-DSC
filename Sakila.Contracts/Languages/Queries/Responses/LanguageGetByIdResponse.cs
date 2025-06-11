bcfa316ae94b482d171bcc9f6972271f11565843
Víctor Blanco <34665275+Blanquitoh@users.noreply.github.com>
2025-06-11 11:10:03 -0400

    Use EditForm with validation

--- Sakila.Contracts/Languages/Queries/Responses/LanguageGetByIdResponse.cs
+++ Sakila.Contracts/Languages/Queries/Responses/LanguageGetByIdResponse.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Sakila.Contracts.Languages.Queries.Responses;
 
 public class LanguageGetByIdResponse
 {
     public int Id { get; init; }
-    public string Name { get; set; } = null!;
+
+    [Required(ErrorMessage = "Language name is required.")]
+    [MaxLength(20, ErrorMessage = "Language name must be 20 characters or fewer.")]
+    public string Name { get; set; } = string.Empty;
 }
