196e2468aacca8d34da8376cdeb1eefbc10be4a8
Victor Blanco - Work <blanquitoh@hotmail.com>
2025-06-11 18:50:39 -0400

    removed unnecesary components

--- Sakila.Web/Pages/Components/GeneralErrors.razor.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Components;
-
-namespace Sakila.Web.Pages.Components;
-
-public partial class GeneralErrors
-{
-    [Parameter] public IEnumerable<string>? Messages { get; set; }
-}
