4abf0d1d46c64e990909fef14c10851d1fe70198
Víctor Blanco <34665275+Blanquitoh@users.noreply.github.com>
2025-06-11 16:32:03 -0400

    Add MudBlazor and migrate UI components

--- Sakila.Web/Layout/NavMenu.razor.cs
+++ Sakila.Web/Layout/NavMenu.razor.cs
@@ -2,11 +2,4 @@ namespace Sakila.Web.Layout;
 
 public partial class NavMenu
 {
-    private bool _collapseNavMenu = true;
-    private string? NavMenuCssClass => _collapseNavMenu ? "collapse" : null;
-
-    private void ToggleNavMenu()
-    {
-        _collapseNavMenu = !_collapseNavMenu;
-    }
 }
--- Sakila.Web/Program.cs
+++ Sakila.Web/Program.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Sakila.Web;
 using Sakila.Web.Extensions;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.AddSakilaServices();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
