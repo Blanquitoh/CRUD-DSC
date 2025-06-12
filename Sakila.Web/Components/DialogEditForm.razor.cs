ccba4d3d10c836ece52640ba5fe7282035ee105f
Víctor M. Blanco N. <blanquitoh@hotmail.com>
2025-06-11 20:31:23 -0400

    ...

--- Sakila.Web/Components/DialogEditForm.razor.cs
+++ Sakila.Web/Components/DialogEditForm.razor.cs
@@ -7,6 +7,6 @@ public partial class DialogEditForm
 {
     [Parameter] public EditContext EditContext { get; set; } = null!;
     [Parameter] public EventCallback OnValidSubmit { get; set; }
-    [Parameter] public RenderFragment? ChildContent { get; set; }
+    [Parameter] public RenderFragment? DialogContent { get; set; }
     [Parameter] public RenderFragment? DialogActions { get; set; }
 }
