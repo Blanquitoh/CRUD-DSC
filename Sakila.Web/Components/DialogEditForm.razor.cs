9af38f1839e669dab20d0c7d1c55ea84173a6daf
Víctor M. Blanco N. <blanquitoh@hotmail.com>
2025-06-11 20:15:17 -0400

    ...

---
+++ Sakila.Web/Components/DialogEditForm.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Sakila.Web.Components;
+
+public partial class DialogEditForm
+{
+    [Parameter] public EditContext EditContext { get; set; } = null!;
+    [Parameter] public EventCallback OnValidSubmit { get; set; }
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+    [Parameter] public RenderFragment? DialogActions { get; set; }
+}
---
+++ Sakila.Web/Components/GeneralValidationSummary.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Sakila.Web.Components;
+
+public partial class GeneralValidationSummary
+{
+    [CascadingParameter] private EditContext CurrentEditContext { get; set; } = null!;
+    private IEnumerable<string> _messages = [];
+
+    protected override void OnInitialized()
+    {
+        if (CurrentEditContext == null)
+        {
+            throw new InvalidOperationException("GeneralValidationSummary must be used inside an EditForm.");
+        }
+
+        CurrentEditContext.OnValidationStateChanged += OnValidationStateChanged;
+        UpdateMessages();
+    }
+
+    private void OnValidationStateChanged(object? sender, ValidationStateChangedEventArgs e)
+    {
+        UpdateMessages();
+        StateHasChanged();
+    }
+
+    private void UpdateMessages()
+    {
+        var identifier = new FieldIdentifier(CurrentEditContext.Model, string.Empty);
+        _messages = CurrentEditContext.GetValidationMessages(identifier);
+    }
+
+    public void Dispose()
+    {
+        CurrentEditContext.OnValidationStateChanged -= OnValidationStateChanged;
+    }
+}
