1ceb9258aa15f648afeebe4866b88c786a533fec
Víctor M. Blanco N. <blanquitoh@hotmail.com>
2025-06-11 21:41:26 -0400

    fixed

---
+++ Sakila.Web/Pages/Countries/List.razor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Sakila.Contracts.Countries.Queries.Responses;
+using Sakila.Web.Abstractions;
+using Sakila.Web.Pages.Countries.Components;
+using Sakila.Web.Extensions;
+
+namespace Sakila.Web.Pages.Countries;
+
+partial class List
+{
+    private ISakilaApiResponse<CountryGetAllResponse>? _getAllResponse;
+
+    [Inject] public ICountryService CountryService { get; set; } = null!;
+    [Inject] public IDialogService DialogService { get; set; } = null!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await RefreshCountries();
+    }
+
+    private async Task RefreshCountries()
+    {
+        await CountryService.GetAllAsync(r => Task.FromResult(_getAllResponse = r));
+    }
+
+    private Task ShowCreateDialog()
+    {
+        return DialogService.ShowDialogAsync<CountryCreateDialog>(
+            "Add Country",
+            onSuccess: RefreshCountries);
+    }
+
+    private Task ShowUpdateDialog(CountryGetByIdResponse country)
+    {
+        var parameters = new DialogParameters { [nameof(CountryUpdateDialog.Country)] = country };
+        return DialogService.ShowDialogAsync<CountryUpdateDialog>(
+            "Edit Country",
+            parameters,
+            RefreshCountries);
+    }
+
+    private Task ShowDeleteDialog(CountryGetByIdResponse country)
+    {
+        var parameters = new DialogParameters { [nameof(ConfirmDelete.Country)] = country };
+        return DialogService.ShowDialogAsync<ConfirmDelete>(
+            "Delete Country",
+            parameters,
+            RefreshCountries);
+    }
+}
---
+++ Sakila.Web/Pages/Languages/List.razor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Sakila.Contracts.Languages.Queries.Responses;
+using Sakila.Web.Abstractions;
+using Sakila.Web.Pages.Languages.Components;
+using Sakila.Web.Extensions;
+
+namespace Sakila.Web.Pages.Languages;
+
+partial class List
+{
+    private ISakilaApiResponse<LanguageGetAllResponse>? _getAllResponse;
+
+    [Inject] public ILanguageService LanguageService { get; set; } = null!;
+    [Inject] public IDialogService DialogService { get; set; } = null!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await RefreshLanguages();
+    }
+
+    private async Task RefreshLanguages()
+    {
+        await LanguageService.GetAllAsync(r => Task.FromResult(_getAllResponse = r));
+    }
+
+    private Task ShowCreateDialog()
+    {
+        return DialogService.ShowDialogAsync<LanguageCreateDialog>(
+            "Add Language",
+            onSuccess: RefreshLanguages);
+    }
+
+    private Task ShowUpdateDialog(LanguageGetByIdResponse language)
+    {
+        var parameters = new DialogParameters { [nameof(LanguageUpdateDialog.Language)] = language };
+        return DialogService.ShowDialogAsync<LanguageUpdateDialog>(
+            "Edit Language",
+            parameters,
+            RefreshLanguages);
+    }
+
+    private Task ShowDeleteDialog(LanguageGetByIdResponse language)
+    {
+        var parameters = new DialogParameters { [nameof(ConfirmDelete.Language)] = language };
+        return DialogService.ShowDialogAsync<ConfirmDelete>(
+            "Delete Language",
+            parameters,
+            RefreshLanguages);
+    }
+}
